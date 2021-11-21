91105041673737a54cbdd975fc5316155871e509
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2021-11-21 23:26:37 +0330

    fix
    Signed-off-by: Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>

--- src/main/java/me/mahdiyar/configuration/security/SecurityConfiguration.java
+++ src/main/java/me/mahdiyar/configuration/security/SecurityConfiguration.java
@@ -109,7 +109,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry
                         .addMapping("/**")
-                        .allowedOrigins("*", "http://localhost:3000")
+                        .allowedOrigins("*", "http://localhost:3000/")
                         .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
             }
         };
@@ -118,7 +118,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*", "http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("*", "http://localhost:3000/"));
         configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
