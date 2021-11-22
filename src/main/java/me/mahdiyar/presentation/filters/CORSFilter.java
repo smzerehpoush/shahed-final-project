c44ae42388f627c1fff6003642bc7ddb7eb9e625
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2021-11-22 21:40:37 +0330

    add cors filter
    Signed-off-by: Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>

---
+++ src/main/java/me/mahdiyar/presentation/filters/CORSFilter.java
@@ -0,0 +1,38 @@
+package me.mahdiyar.presentation.filters;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CORSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+//        no implementation
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String requestOrigin = ((HttpServletRequest) servletRequest).getHeader("Origin");
+        if (requestOrigin != null) {
+            response.setHeader("Access-Control-Allow-Origin", requestOrigin);
+        }
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers",
+                "X-Requested-With, Content-Type, Origin, Authorization");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Expose-Headers", "Content-Type, Origin, Accept, X-Requested-With, Authorization");
+        filterChain.doFilter(servletRequest, response);
+    }
+
+    @Override
+    public void destroy() {
+//        no implementation
+    }
+}
